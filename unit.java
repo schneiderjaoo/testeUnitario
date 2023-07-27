eb568b6b7ff91fe1c00878d1208c87dc3f2ca76e
João Schneider <126828193+schneiderjaoo@users.noreply.github.com>
2023-07-27 13:19:44 -0300

    Create unit.java

---
+++ unit.java
@@ -0,0 +1,11 @@
+public class soma {
+
+    public void Somatest() {
+        
+        int a = 5;
+        int b = 3;
+
+        int result = soma.add(a, b);
+
+        assertEquals(8, result);
+    }
