856557a683750ee9a885a14fca5063f3ad33f63e
João Schneider <126828193+schneiderjaoo@users.noreply.github.com>
2023-07-27 13:18:26 -0300

    Update unit.py

--- unit.py
+++ unit.py
@@ -1,1 +1,18 @@
+import unittest
 
+def soma(a, b);
+    return a + b
+
+class TestConta(unittest.TestCase);
+
+    def TestConta(self);
+      
+        a = 5
+        b = 3
+
+        result = soma(a, b)
+
+        self.assertEqual(result, 8)
+      
+if __name__ == '__main__';
+    unittest.main()
